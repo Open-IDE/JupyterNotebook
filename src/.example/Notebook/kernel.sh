9c05977a1bf25ae5598b4bb7f524a7205dce1b5d
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-07-31 13:16:43 -0500

    .example

--- src/.example/Notebook/kernel.sh
+++ src/.example/Notebook/kernel.sh
@@ -1,4 +1,5 @@
-python -m venv venv
-venv\Scripts\activate
-python -m pip install ipykernel
-python -m ipykernel install --user --name=myproject"
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install ipykernel
+# python3 -m ipykernel install --user --name=myproject
+jupyter-notebook
